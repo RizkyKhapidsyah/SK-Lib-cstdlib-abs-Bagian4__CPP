aaacbfd641004b2280abfb107fbcf7805b91372f
Programmer Pemula <45317201+RizkyKhapidsyah@users.noreply.github.com>
2023-09-23 10:04:31 +0700

    Initial commit

---
+++ SK-Lib-cstdlib-abs-Bagian4__CPP/Source.cpp
@@ -0,0 +1,19 @@
+#include <climits>
+#include <cstdlib>
+#include <iostream>
+#include <conio.h>
+
+/*
+    Source by CPPReference (https://en.cppreference.com)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+    std::cout << std::showpos
+        << "abs(+3) = " << std::abs(3) << '\n'
+        << "abs(-3) = " << std::abs(-3) << '\n';
+
+    _getch();
+    return 0;
+}
